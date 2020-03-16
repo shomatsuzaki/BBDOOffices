84be19b399e18573cc774b379f8efb8cd5d60b6c
Sho Matsuzaki <sho.g.matsuzaki@gmail.com>
2020-03-16 12:25:55 -0400

    Added gradient background and basic styling

--- js/index.js
+++ js/index.js
@@ -3,26 +3,28 @@ $(document).ready(function() {
 	var context = canvas.getContext('2d');
 	var officeName = '';
 	var dataURL = '';
-	var img = document.getElementById('canvasimg');
+	// var img = document.getElementById('canvasimg');
 	var logo = document.getElementById('bbdo');
 
-	let f = new FontFace('GothamUltra', 'url(./fonts/Gotham-Ultra.woff)');
-	f.family = 'GothamUltra';
+	let f = new FontFace('GothamBlack', 'url(./fonts/Gotham-Black.woff)');
+	f.family = 'GothamBlack';
 	f.load().then(function() {
-		context.font = '245px GothamUltra';
+		context.font = '245px GothamBlack';
 		context.textAlign = 'start';
 
-		$('#btnoffice').click(function() {
-			officeName = document.getElementById('officeform').value.toUpperCase();
+		$('#go').click(function() {
+			var officeform = document.getElementById('officeform');
+			officeform.style.display = 'none';
+			document.getElementById('go').style.display = 'none';
+			officeName = officeform.value.toUpperCase();
 			context.clearRect(0, 0, canvas.width, canvas.height);
 			context.fillText(officeName, 850, (canvas.height/2)+63);
 			context.drawImage(logo, 200, (canvas.height/2)-110);
 
 			//Convert canvas to visible image
-			dataURL = canvas.toDataURL("image/png;base64");
-			img.src = dataURL;
-
-			img.style.display = 'block';
+			// dataURL = canvas.toDataURL("image/png;base64");
+			// img.src = dataURL;
+			// img.style.display = 'block';
 			document.getElementById('btndownload').style.display = 'block';
 		});
 
@@ -38,13 +40,83 @@ $(document).ready(function() {
 	});
 });
 
+////////////////////////
+// Gradient background
+////////////////////////
 
-// Check mobile or desktop
+var colors = new Array(
+[62,35,255],
+[60,255,60],
+[255,35,98],
+[45,175,230],
+[255,0,255],
+[255,128,0]);
+
+var step = 0;
+//color table indices for: 
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
+
+//transition speed
+var gradientSpeed = 0.002;
+
+function updateGradient() {
+	if ( $===undefined ) return;
+  
+	var c0_0 = colors[colorIndices[0]];
+	var c0_1 = colors[colorIndices[1]];
+	var c1_0 = colors[colorIndices[2]];
+	var c1_1 = colors[colorIndices[3]];
+
+	var istep = 1 - step;
+	var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+	var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+	var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+	var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+	var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+	var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+	var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+	var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+	$('#gradient').css({
+		background: "-webkit-linear-gradient(150deg, "+color1+" 0%, "+color2+" 100%)"})
+		.css({background: "-moz-linear-gradient(150deg, "+color1+" 0%, "+color2+" 100%)"})
+		.css({background: "linear-gradient(150deg, "+color1+" 0%, "+color2+" 100%)"});
+  
+	step += gradientSpeed;
+	if ( step >= 1 ) {
+	    step %= 1;
+	    colorIndices[0] = colorIndices[1];
+	    colorIndices[2] = colorIndices[3];
+	    
+	    //pick two new target color indices
+	    //do not pick the same as the current one
+	    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+	    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+	}
+}
+
+setInterval(updateGradient,10);
+
+
+////////////////////////
+// Check for mobile
+////////////////////////
+
 window.mobileAndTabletcheck = function() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 }();
+
+
+////////////////////////
+// Download PNG from canvas
+////////////////////////
 
 // Source from:  http://stackoverflow.com/questions/18480474/how-to-save-an-image-from-canvas
 
